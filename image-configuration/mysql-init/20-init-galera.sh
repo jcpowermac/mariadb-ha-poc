4c8e2b2236b9f020c17a979c543582c371bc2aa2
Joseph Callen <jcallen@redhat.com>
2018-01-25 13:25:57 -0500

    changed name of string and added better echo

--- image-configuration/mysql-init/20-init-galera.sh
+++ image-configuration/mysql-init/20-init-galera.sh
@@ -2,7 +2,7 @@
 
 if [ -v INIT_GALERA ];
     then
-        echo "Shutting down mariadb"
+        echo "Init galera finished - shutting down mariadb"
         shutdown_local_mysql
         # should be done and we don't want the script to continue
         exit 0
