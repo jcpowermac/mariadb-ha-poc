0663395d9edaf11038422edcfe0c1f6e7bb52f89
Joseph Callen <jcallen@redhat.com>
2018-02-12 15:46:03 -0500

    testing

--- image-configuration/mysql-init/80-init-galera.sh
+++ image-configuration/mysql-init/80-init-galera.sh
@@ -9,6 +9,27 @@ if [ -v INIT_GALERA ];
 fi
 
 WSREP_ON=1
+NAMESPACE="rhscl-mariadb"
+
+# Retrieve the SRV records from DNS
+SRVSTRING=`host -t SRV ${NAMESPACE}`
+FQDN=()
+
+readarray -t SRV <<< "$SRVSTRING"
+
+for entry in "${SRV[@]}"
+do
+    temp=$(echo $entry | awk '{print $8}')
+    FQDN+=("${temp:0:-1}")
+done
+
+if [ "${#FQDN[@]}" = 1 ]; then
+    NODES=""
+else
+    NODES=$(IFS=,; echo "${FQDN[*]}")
+fi
+
+export NODES
 export WSREP_ON
 
 process_extending_config_files ${APP_DATA}/mysql-cfg/ ${CONTAINER_SCRIPTS_PATH}/cnf/
