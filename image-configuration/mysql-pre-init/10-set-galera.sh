b0bd94958f46987d3477696bda8574d92b668d7d
Joseph Callen <jcallen@redhat.com>
2018-02-12 16:18:49 -0500

    update scripts

--- image-configuration/mysql-pre-init/10-set-galera.sh
+++ image-configuration/mysql-pre-init/10-set-galera.sh
@@ -6,27 +6,27 @@ set -x
 #NAMESPACE=`cat /run/secrets/kubernetes.io/serviceaccount/namespace`
 
 # THIS IS WRONG ***
-NAMESPACE="rhscl-mariadb"
+#NAMESPACE="rhscl-mariadb"
 WSREP_ON=0
 
 # Retrieve the SRV records from DNS
-SRVSTRING=`host -t SRV ${NAMESPACE}`
-FQDN=()
-
-readarray -t SRV <<< "$SRVSTRING"
-
-for entry in "${SRV[@]}"
-do
-    temp=$(echo $entry | awk '{print $8}')
-    FQDN+=("${temp:0:-1}")
-done
-
-if [ "${#FQDN[@]}" = 1 ]; then
-    NODES=""
-else
-    NODES=$(IFS=,; echo "${FQDN[*]}")
-fi
-
-
-export NODES
+#SRVSTRING=`host -t SRV ${NAMESPACE}`
+#FQDN=()
+#
+#readarray -t SRV <<< "$SRVSTRING"
+#
+#for entry in "${SRV[@]}"
+#do
+#    temp=$(echo $entry | awk '{print $8}')
+#    FQDN+=("${temp:0:-1}")
+#done
+#
+#if [ "${#FQDN[@]}" = 1 ]; then
+#    NODES=""
+#else
+#    NODES=$(IFS=,; echo "${FQDN[*]}")
+#fi
+#
+#
+#export NODES
 export WSREP_ON
